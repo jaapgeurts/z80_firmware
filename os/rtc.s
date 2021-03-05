499d7393a43e75ead792a965aaf167de402b7121
Jaap Geurts <jaapgeurts@fontys.nl>
2021-03-05 15:52:26 +0100

    Improved code for inclusion in firmware

--- os/rtc.s
+++ os/rtc.s
@@ -3,6 +3,8 @@ RTC    equ 0x20
 CR equ 0x0D
 LF equ 0x0A
 
+RTC_REG_COUNT equ 0x0d
+
 GETC     equ 0x0008 ; RST 1 getKey
 PUTC     equ 0x0010 ; RST 2 putSerialChar
 PRINTK   equ 0x0018 ; RST 3 printk
@@ -13,16 +15,31 @@ READLINE equ 0x0020 ; RST 4 readline
 
 start:
 
+  push hl
+  push bc
+
   ld   hl, hello_msg
   rst  PRINTK ; call printk
 
-  ; code for accessing rtc
-  push bc
-  push hl
+  ld   hl,v_timestruct
+  ;call RTCInit
+  call RTCRead
 
-  jp   ReadRTC
+  ld   b,6
+again:
+  ld   a,(hl)
+  inc  hl
+  add  '0'
+  rst  PUTC
+  djnz again
+
+  pop  bc
+  pop  hl
+  ret
 
 RTCInit:
+  push bc
+  push hl
 
 ; start counting
   ld   a,0b00000000 ; test=0, 24hr, stop=0, reset=0
@@ -42,12 +59,13 @@ RTCInit:
   out  (RTC+0x0f),a
 
 ; set current time
-  ld   a,0b00000000 ; set all counts to 0
-  ld   b,0x0d
+  ld   b,RTC_REG_COUNT
   ld   c,RTC
 .RTCInit_next:
+  ld   a,(hl)
   out  (c),a
   inc  c
+  inc  hl
   djnz .RTCInit_next
 
 ; start counter and release hold
@@ -56,29 +74,32 @@ RTCInit:
   ld   a,0b00000000 ; 30s-adj=0, irq=0, busy=0,hold=0
   out  (RTC+0x0d),a
 
+  pop  hl
+  pop  bc
+  ret
 ; done init
 
-ReadRTC:
-  ld   b,6
-  ld   c,RTC+5
+RTCRead:
+
+  push bc
+  push hl
+
+  ld   b,RTC_REG_COUNT
+  ld   c,RTC
   call RTCCheckBusy
 
-.ReadRTC_loop:
+.RTCRead_loop:
 
   in   a,(c) ;
-
   and  0x0F ; chop off high nibble
-  add  '0'
-  rst  PUTC ; call putSerialChar
-  dec  c
-  djnz .ReadRTC_loop
+  ld   (hl),a
+  inc  c
+  inc  hl
+  djnz .RTCRead_loop
 
 ; release hold
   ld   a,0b00000000 ; 30s-adj=0, irq=0, busy=0,hold=0
   out  (RTC+0x0d),a
-
-  ld   hl,newline
-  rst  PRINTK ; call printk
 
   pop  hl
   pop  bc
@@ -103,5 +124,9 @@ RTCCheckBusy:
 
 hello_msg: ascii 11,"Date test: "
 newline:   db 3,CR,LF
+
+  org 0x8100
+v_timestruct:  db 0,0,0,4,5,1,5,0,0,3,1,2,5
+
 
 ;; PS2/ scancode set 2
